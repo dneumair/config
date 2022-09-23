6fe6f39ff87b77e0631a386eb39518f5349f0c91
David Neumair <dn@honeysuckle.dev>
2022-09-23 23:47:03 +0200

    setup minimal packer and mason

--- nvim/lua/keymapping.lua
+++ nvim/lua/keymapping.lua
@@ -3,4 +3,5 @@ vim.g.mapleader = "<space>"
 
 -- some basic remaps for german quertz keyboard
 vim.keymap.set( 'n','ä',':' ,{noremap=true,silent=false} ) --
+vim.keymap.set( 'n','ö',';' ,{noremap=true,silent=true} ) --
 
