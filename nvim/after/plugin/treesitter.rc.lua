2d79a1ed67ea26046c4fc81dfefa336dc457fde9
David Neumair <dn@honeysuckle.dev>
2023-01-24 17:08:13 +0100

    deleted treesitter gitignore and gitcommit languages

--- nvim/after/plugin/treesitter.rc.lua
+++ nvim/after/plugin/treesitter.rc.lua
@@ -1,7 +1,7 @@
 local loaded, ts_conf = pcall(require,"nvim-treesitter.configs")
 if loaded then
     ts_conf.setup {
-    ensure_installed = {"vim", "c", "yaml", "json", "lua", "rust", "go", "gomod","gitignore", "gitcommit"},
+    ensure_installed = {"vim", "c", "yaml", "json", "lua", "rust", "go", "gomod"},
     highlight = {
         enable = true,
     },
